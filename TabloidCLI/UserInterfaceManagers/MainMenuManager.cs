20e5c909b6102c4e91c125510eff8336824fc026
cslunetta <cslunetta@outlook.com>
2021-03-22 10:32:12 -0500

    changed color theme to option 7

--- TabloidCLI/UserInterfaceManagers/MainMenuManager.cs
+++ TabloidCLI/UserInterfaceManagers/MainMenuManager.cs
@@ -27,7 +27,7 @@ namespace TabloidCLI.UserInterfaceManagers
             Console.WriteLine(" 4) Post Management");
             Console.WriteLine(" 5) Tag Management");
             Console.WriteLine(" 6) Search by Tag");
-            Console.WriteLine(" *) Color Themes");
+            Console.WriteLine(" 7) Color Themes");
             Console.WriteLine(" 0) Exit");
 
             Console.Write("> ");
@@ -43,7 +43,7 @@ namespace TabloidCLI.UserInterfaceManagers
                 case "0":
                     Console.WriteLine("Good bye");
                     return null;
-                case "*": Console.Clear(); return new ColorManager(this);
+                case "7": Console.Clear(); return new ColorManager(this);
                 default:
                     Console.WriteLine("Invalid Selection");
                     return this;
